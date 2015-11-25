a812f7e4f2967dbd2cbac4428288316488530007
Andrew Dodson <sonofdod@gmail.com>
2015-11-25 18:58:43 +0000

    Update oauth-shim

--- credentials.js
+++ credentials.js
@@ -3,28 +3,28 @@ require('dotenv').load();
 module.exports = [
 	{
 		"name": "github",
-		"domain": "localhost",
+		"domain": "localhost hellojs-signin-demo.herokuapp.com",
 		"client_id": process.env.GITHUB_ID,
 		"client_secret": process.env.GITHUB_SECRET,
 		"grant_url": "https://github.com/login/oauth/access_token"
 	},
 	{
 		"name": "google",
-		"domain": "localhost",
+		"domain": "localhost hellojs-signin-demo.herokuapp.com",
 		"client_id": process.env.GOOGLE_ID,
 		"client_secret": process.env.GOOGLE_SECRET,
 		"grant_url": "https://accounts.google.com/o/oauth2/token"
 	},
 	{
 		"name": "twitter",
-		"domain": "",
+		"domain": "localhost hellojs-signin-demo.herokuapp.com",
 		"client_id": process.env.TWITTER_ID,
 		"client_secret": process.env.TWITTER_SECRET,
 		"grant_url": "https://api.twitter.com/oauth/access_token"
 	},
 	{
 		"name": "windows",
-		"domain": "local.knarly.com",
+		"domain": "local.knarly.com hellojs-signin-demo.herokuapp.com",
 		"client_id": process.env.WINDOWS_ID,
 		"client_secret": process.env.WINDOWS_SECRET,
 		"grant_url": "https://login.live.com/oauth20_token.srf"
